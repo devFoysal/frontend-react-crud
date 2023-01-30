ad4cecda41f329bb57a37091ac7467150fdddbbe
Foysal Mahmud <dev.foysal@gmail.com>
2023-01-30 22:30:19 +0600

    readme file update

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,4 @@
 export const BASE_URL = process.env.PUBLIC_URL;
 
-const live = 0;
-export const API_URL = live ? "https://mabroor.showtalent.org/api/v1" : "http://localhost:8000/api/v1";
+export const API_URL =  "http://localhost:8000/api/v1";
 export const IMAGE_URL = "http://localhost:8000/products";
